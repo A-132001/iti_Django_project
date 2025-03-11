2a7d5f0a3d38af1298ed6620faa63d39854cc026
Abdelrahman Sabry <abdelrahmansabry53@gmail.com>
2025-03-11 03:09:05 +0200

    Adding CRUD operation of the Trainee(create,get,update,delete)

--- trainee/views.py
+++ trainee/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from .models import Trainee 
 
 def TraineeList(req):
     context = {}
-    context["all_trainees"] = Trainee.objects.all()
+    # context["all_trainees"] = Trainee.objects.all()
+    context["all_trainees"] = Trainee.objects.filter(isactive=True)
     return render(req,"trainee/showtrainees.html",context)
 
 def AddTrainee(req):
+    if(req.method == "POST"):
+        Trainee.objects.create(name=req.POST["trname"],
+                               email=req.POST["tremail"],
+                               image=req.FILES["trimage"],)
     return render(req,"trainee/addtrainee.html")
 
 def UpdateTrainee(req,id):
-    return render(req,"trainee/updatetrainee.html")
-
+    trainee = Trainee.objects.get(id=id)
+    context = {"trainee":trainee}
+    if(req.method == "POST"):
+        Trainee.objects.filter(id=id).update(name=req.POST["trname"],
+                               email=req.POST["tremail"],
+                               image=req.FILES["trimage"],)
+        return redirect('trainee_list')
+    return render(req, 'trainee/updatetrainee.html',context)
 def DeleteTrainee(req,id):
-    return render(req,"trainee/deletetrainee.html")
+    trainee = Trainee.objects.get(id=id)
+    trainee.isactive=False
+    trainee.save()
+    return redirect("trainee_list")
